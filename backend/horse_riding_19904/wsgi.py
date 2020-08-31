935193cbe96f457d21ee356c4b06f021570514a2
Crowdbotics <team@crowdbotics.com>
2020-08-31 20:42:44 +0000

    Initial commit.

---
+++ backend/horse_riding_19904/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for horse_riding_19904 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'horse_riding_19904.settings')
+
+application = get_wsgi_application()
